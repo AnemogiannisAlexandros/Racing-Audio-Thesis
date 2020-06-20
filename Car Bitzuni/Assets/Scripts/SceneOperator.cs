b3b8a6b2751884ccfb46db36794b8fd8f55a6b1a
Alexandros Anemogiannis <alex.anemogiannis@hotmail.com>
2020-06-20 15:36:09 +0300

    Scene Sequencing Rework,Checkpoint System Extra Tracks

--- Car Bitzuni/Assets/Scripts/SceneOperator.cs
+++ Car Bitzuni/Assets/Scripts/SceneOperator.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class SceneOperator : MonoBehaviour
 {
     public static SceneOperator Instance { get; private set; }
     public SceneAsset[] scenes;
-    public AudioClip[] tracks = new AudioClip[2];
+    public List<AudioClip> fastTracks = new List<AudioClip>(2);
+    public List<AudioClip> slowTracks = new List<AudioClip>(2);
+    public List<AudioClip> tracksArranged = new List<AudioClip>();
     private SceneAsset[] chosenScenes = new SceneAsset[2];
     public UnityEvent OnTrackFinished;
     private scenesTracked[] scenesTrackeds;
@@ -35,8 +39,41 @@ public class SceneOperator : MonoBehaviour
     {
         scenesTrackeds = new scenesTracked[2];
         ChooseCandidateScenes();
+        ArangeTracks();
     }
-    void ChooseCandidateScenes() 
+    void ArangeTracks() 
+    {
+        int firstTrackRandom = Random.Range(0, 2);
+        if (firstTrackRandom == 0)
+        {
+            for (int i = 0; i <= 1; i++)
+            {
+                int clipIndex = Random.Range(0, fastTracks.Count);
+                tracksArranged.Add(fastTracks[clipIndex]);
+                fastTracks.RemoveAt(clipIndex);
+                clipIndex = Random.Range(0, slowTracks.Count);
+                tracksArranged.Add(slowTracks[clipIndex]);
+                slowTracks.RemoveAt(clipIndex);
+            }
+        }
+        else 
+        {
+            for (int i = 0; i <= 1; i++)
+            {
+                int clipIndex = Random.Range(0, slowTracks.Count);
+                tracksArranged.Add(slowTracks[clipIndex]);
+                slowTracks.RemoveAt(clipIndex);
+                clipIndex = Random.Range(0, fastTracks.Count);
+                tracksArranged.Add(fastTracks[clipIndex]);
+                fastTracks.RemoveAt(clipIndex);
+            }
+        }
+        for (int i = 0; i <= tracksArranged.Count - 1; i++)
+        {
+            Debug.Log(tracksArranged[i].name);
+        }
+    }
+    void ChooseCandidateScenes()  
     {
         for (int i = 0; i < 2; i++) 
         {
@@ -46,10 +83,8 @@ public class SceneOperator : MonoBehaviour
         {
             chosenScenes[1] = scenes[Random.Range(0, 4)];
         }
-        for (int i = 0; i < 2; i++)
-        {
-            Debug.Log(chosenScenes[0].name + "  " + chosenScenes[1].name);
-        }
+
+        Debug.Log(chosenScenes[0].name + "  " + chosenScenes[1].name);
     }
     public void LoadNewScene() 
     {
@@ -57,35 +92,35 @@ public class SceneOperator : MonoBehaviour
         {
             OnTrackFinished.Invoke();
         }
-        if (scenesTrackeds[0].scene == null)
+
+        if (scenesTrackeds[0].timesPlayed == 0)
         {
-            if (scenesTrackeds[0].timesPlayed == 0)
-            {
-                SceneManager.LoadScene(chosenScenes[0].name);
-                AudioManager.Instance.LoadTrack(tracks[0]);
-                scenesTrackeds[0].timesPlayed = 1;
-            }
-            else
-            {
-                SceneManager.LoadScene(chosenScenes[0].name);
-                AudioManager.Instance.LoadTrack(tracks[1]);
-                scenesTrackeds[0].scene = chosenScenes[0];
-            }
+            SceneManager.LoadScene(chosenScenes[0].name);
+            AudioManager.Instance.LoadTrack(tracksArranged[0]);
+            scenesTrackeds[0].timesPlayed = 1;
         }
-        else
+        else if (scenesTrackeds[1].timesPlayed == 0)
         {
-            if (scenesTrackeds[1].timesPlayed == 0)
-            {
-                SceneManager.LoadScene(chosenScenes[1].name);
-                AudioManager.Instance.LoadTrack(tracks[0]);
-                scenesTrackeds[1].timesPlayed = 1;
-            }
-            else
-            {
-                SceneManager.LoadScene(chosenScenes[1].name);
-                AudioManager.Instance.LoadTrack(tracks[1]);
-                scenesTrackeds[1].scene = chosenScenes[1];
-            }
+            SceneManager.LoadScene(chosenScenes[1].name);
+            AudioManager.Instance.LoadTrack(tracksArranged[1]);
+            scenesTrackeds[1].timesPlayed = 1;
+        }
+        else if (scenesTrackeds[0].timesPlayed == 1)
+        {
+            SceneManager.LoadScene(chosenScenes[0].name);
+            AudioManager.Instance.LoadTrack(tracksArranged[2]);
+            scenesTrackeds[0].timesPlayed = 2;
+
+        }
+        else if (scenesTrackeds[1].timesPlayed == 1)
+        {
+            SceneManager.LoadScene(chosenScenes[1].name);
+            AudioManager.Instance.LoadTrack(tracksArranged[3]);
+            scenesTrackeds[1].timesPlayed = 2;
+        }
+        else 
+        {
+            SceneManager.LoadScene("ThankYou");
         }
         DataManager.Instance.Init();
     }
